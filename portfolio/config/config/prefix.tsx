c8f088ae4685948bc72045ef113c70b321083949
yeni28 <suntofu0228@naver.com>
2023-07-18 07:27:53 +0900

    fix : fix prefix file

--- portfolio/config/config/prefix.tsx
+++
@@ -1,1 +0,0 @@
-export const prefix = process.env.NODE_ENV === 'production' ? 'http://yeni28.github.io/yeni28.github.io' : '';
--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '@/components/Navbar';
-import { PortfolioProvider } from '../components/context/context';
-import { prefix } from '../../config/config';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PortfolioProvider value={{ prefix }}>
-      <div>
-        <Navbar />
-        <Component {...pageProps} />
-      </div>
-    </PortfolioProvider>
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
   );
 }

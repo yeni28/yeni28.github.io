8e3f19383d090c055af88dfbe07192ca4d77ee59
yeni28 <suntofu0228@naver.com>
2023-07-18 20:57:14 +0900

    fix: prefix url

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,1 +1,1 @@
-export const prefix = process.env.NODE_ENV === 'production' ? 'https://yeni28.github.io/yeni28.github.io' : '';
+export const prefix = process.env.NODE_ENV === 'production' ? 'https://yeni28.github.ios' : '';

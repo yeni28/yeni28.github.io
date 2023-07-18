eef51d41460fb56f3b719b0b44342629dfe6afe5
yeni28 <suntofu0228@naver.com>
2023-07-18 21:20:51 +0900

    fix : tailwindcss add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 import Navbar from '@/components/Navbar';
 import { PortfolioProvider } from '../components/context/context';
 import { prefix } from '../components/config/config';
+import 'tailwindcss/tailwind.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

b073027541edf635cb205f80df72effc041223e5
yeni28 <suntofu0228@naver.com>
2023-07-21 22:14:03 +0900

    배포0.1

--- yenportfolio/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss';
-
-export default defineConfig({
-  // base: "/yeni28.github.io/",
-  plugins: [react(),],
-})
